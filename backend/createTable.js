94fc1090cc41fe0c8960fb7f09752333c70557b7
adriastephanie <adriastephanie@hotmail.com>
2025-02-27 14:46:56 -0300

    remove regiao

--- backend/createTable.js
+++ backend/createTable.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk');
 require('dotenv').config();
 
 AWS.config.update({
-  region: process.env.AWS_REGION || 'us-east-1',
+  region: process.env.AWS_REGION
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
